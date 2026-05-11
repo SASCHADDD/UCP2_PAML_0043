f4d8806ae715920d372a792a08ea067f3253de97
SASCHADDD <saschadanu23@gmail.com>
2026-05-12 00:42:23 +0700

    menambahkan validasi untuk memeriksa keberadaan mobil berdasarkan plat nomor

--- service/katalog/Katalog.service.js
+++ service/katalog/Katalog.service.js
@@ -4,4 +4,9 @@ exports.tambahKatalog = async (data) => {
     const { nama_kendaraan, merk , tahun, plat_nomor,harga, status} = data;
 
     const [ExistingCar] = await db.query('SELECT * FROM katalog WHERE plat_nomor = ?',[plat_nomor]);
+    if(ExistingCar.length>0){
+        const error = new Error('Plat nomor sudah terdaftar di mobil!');
+        error.statusCode = 400;
+        throw error;
+    }
 }
