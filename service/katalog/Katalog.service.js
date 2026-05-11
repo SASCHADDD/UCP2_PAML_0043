9779a3a43360151d5069fc9a916c9a68b8c3ab49
SASCHADDD <saschadanu23@gmail.com>
2026-05-12 00:37:36 +0700

    menambahkan deklarasi ExistingCar pada method tambahKatalog

--- service/katalog/Katalog.service.js
+++ service/katalog/Katalog.service.js
@@ -2,4 +2,6 @@ const db = require('../../config/db');
 
 exports.tambahKatalog = async (data) => {
     const { nama_kendaraan, merk , tahun, plat_nomor,harga, status} = data;
+
+    const [ExistingCar]
 }
