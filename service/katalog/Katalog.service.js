429e423733dbfd1e73ff3232315ca5867c2badb2
SASCHADDD <saschadanu23@gmail.com>
2026-05-12 00:39:33 +0700

    menambahkan query untuk memeriksa keberadaan mobil berdasarkan plat nomor

--- service/katalog/Katalog.service.js
+++ service/katalog/Katalog.service.js
@@ -3,5 +3,5 @@ const db = require('../../config/db');
 exports.tambahKatalog = async (data) => {
     const { nama_kendaraan, merk , tahun, plat_nomor,harga, status} = data;
 
-    const [ExistingCar]
+    const [ExistingCar] = await db.query('SELECT * FROM katalog WHERE plat_nomor = ?',)
 }
