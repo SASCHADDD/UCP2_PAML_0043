206cdccabca84f345e8d0176f79974df980a1563
SASCHADDD <saschadanu23@gmail.com>
2026-05-13 11:06:33 +0700

    mmebuat model dari auth menyelaraskan nama tabel tabel di database dan json nya di backend untuk sinkronisasi API

---
+++ lib/data/models/auth/authmodel.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+
+class UserModel extends Equatable {
+  final String id;
+  final String nama;
+  final String email;
+  final String password;
+
+  const UserModel({
+    required this.id,
+    required this.nama,
+    required this.email,
+    required this.password,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json){
+    return UserModel(
+    id: json ['id']??'',
+    nama: json['nama']??'',
+    email: json['email']??'',
+    password: json['password']??''
+     );
+  }
+  @override
+  List<Object?> get props => [
+    id, 
+    nama,
+    email,
+    password
+    ];
+}
