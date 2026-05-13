af1de1345f8b06ef6e28c2f998794e12716ea527
SASCHADDD <saschadanu23@gmail.com>
2026-05-13 11:40:34 +0700

    mmebuat model kategori

--- lib/data/models/kategori/kategorimodel.dart
+++ lib/data/models/kategori/kategorimodel.dart
@@ -1,1 +1,16 @@
-class 
+class KategoriModel {
+  final String idKategori;
+  final String namaKategori;
+
+  KategoriModel({
+    required this.idKategori, 
+    required this.namaKategori
+    });
+
+  factory KategoriModel.fromJson(Map<String, dynamic> json) {
+    return KategoriModel(
+      idKategori: json['id_kategori'],
+      namaKategori: json['nama_kategori'],
+    );
+  }
+}
