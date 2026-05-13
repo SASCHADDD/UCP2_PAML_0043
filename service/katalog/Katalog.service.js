d7ecf8521d131679234ff0b76b367c1fc3cdc792
SASCHADDD <saschadanu23@gmail.com>
2026-05-13 18:19:24 +0700

    perbaiki fungsi tambahKatalog untuk menyimpan gambar dan status mobil

--- service/katalog/Katalog.service.js
+++ service/katalog/Katalog.service.js
@@ -1,7 +1,7 @@
 const db = require('../../config/db');
 
 exports.tambahKatalog = async (data) => {
-    const { id_kategori, nama_kendaraan, merk , tahun, plat_nomor,harga, status} = data;
+    const { id_kategori, nama_kendaraan, merk , tahun, plat_nomor,harga, status, gambar } = data;
 
     const [ExistingCar] = await db.query('SELECT * FROM katalog WHERE plat_nomor = ?',[plat_nomor]);
     if(ExistingCar.length>0){
@@ -11,12 +11,13 @@ exports.tambahKatalog = async (data) => {
     }
 
     const [result] = await db.query(
-        'INSERT INTO katalog (id_kategori, nama_kendaraan, merk, tahun, plat_nomor, harga) VALUES (?, ?, ?, ?, ?, ?)',
-        [id_kategori, nama_kendaraan, merk, tahun, plat_nomor, harga]
+        'INSERT INTO katalog (id_kategori, nama_kendaraan, merk, tahun, plat_nomor, harga, status, gambar) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+        [id_kategori, nama_kendaraan, merk, tahun, plat_nomor, harga, status, gambar]
     );
     return { 
         message: 'Data mobil berhasil ditambahkan!', 
-        id_katalog: result.insertId 
+        id_katalog: result.insertId,
+        gambar: gambar
     };
 }
 
