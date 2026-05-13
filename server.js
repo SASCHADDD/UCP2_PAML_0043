00d2b1c3c21f53ff416e943e750e92b4d5a84662
SASCHADDD <saschadanu23@gmail.com>
2026-05-13 18:18:43 +0700

    perbaiki pengaturan middleware untuk menghubungkan folder uploads dengan path statis

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const path = require('path');
 const app = express();
 const authRoutes = require('./routes/auth/Auth.route');
 const katalogRoutes = require('./routes/katalog/Katalog.route'); 
@@ -10,10 +10,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 
-// Menghubungkan prefix '/api/auth' dengan authRoutes
 app.use('/api/auth', authRoutes);
+
 app.use('/api/katalog', katalogRoutes); 
+
 app.use('/api/kategori', kategoriRoutes);
 // Route dasar
 app.get('/', (req, res) => {
