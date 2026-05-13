b530c6c46c03cc1eb0fc24245a7dbd4a17798777
SASCHADDD <saschadanu23@gmail.com>
2026-05-13 11:25:48 +0700

    membuat model katalog dengan menyesuaikan dalam map dengan json

---
+++ lib/data/models/katalog/katalogmodel.dart
@@ -0,0 +1,33 @@
+import 'package:equatable/equatable.dart';
+
+class KatalogModel extends Equatable {
+  final String idKatalog;
+  final String namaKendaraan;
+  final String merk;
+  final String tahun;
+  final String platNomor;
+  final String harga;
+  final String status;
+
+  KatalogModel({
+    required this.idKatalog,
+    required this.namaKendaraan,
+    required this.merk,
+    required this.tahun,
+    required this.platNomor,
+    required this.harga,
+    required this.status,
+  });
+
+  factory KatalogModel.fromJson(Map<String, dynamic> json) {
+    return KatalogModel(
+      idKatalog: json['id_katalog'],
+      namaKendaraan: json['nama_kendaraan'], 
+      merk: json['merk'],
+      tahun: json['tahun'],
+      platNomor: json ['platNomor'],
+      harga: json ['harga'],
+      status: json['status']
+    );
+  }
+}
